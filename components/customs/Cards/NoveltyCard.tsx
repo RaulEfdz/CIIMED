ca10825c2ca589acd83c3db7770d09e6dcc45bda
raulefdz <raulefdz@gmail.com>
2025-01-31 17:36:33 -0500

    rounded-none

--- components/customs/Cards/NoveltyCard.tsx
+++ components/customs/Cards/NoveltyCard.tsx
@@ -9,7 +9,8 @@ export interface NoveltyCardProps {
 
 const NoveltyCard: React.FC<NoveltyCardProps> = ({ title, description, category, date }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200">
+    <div className="bg-white shadow-md rounded-none
+     p-4 border border-gray-200">
       <h3 className="text-xl font-bold text-gray-900">{title}</h3>
       <p className="text-gray-600 mt-2">{description}</p>
       <div className="mt-4 flex justify-between items-center text-sm text-gray-500">
