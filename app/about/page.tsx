e2c31f47a080d738e84776f682e91dd2d1750604
raulefdz <raulefdz@gmail.com>
2025-01-31 21:11:56 -0500

    ddd

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 
-import { Construction } from "lucide-react";
+import AboutUs from "@/components/customs/Heros/HeroAboutUs";
+
+const About = () => {
+  return (
+    <>
+      <AboutUs
+        title="Sobre Nosotros"
+        description="CIIMED es un centro de investigación líder en el desarrollo de innovaciones médicas y científicas."
+        imageUrl="jcue8atmcjfv9aea2sw0"
+        imageAlt="Equipo de investigación en CIIMED"
+        imgW={2048}
+        imgH={1365}
+        mission="Somos un centro líder en investigación médica, comprometido con la excelencia científica y la innovación en salud pública. Nuestra misión es generar conocimiento y evidencia científica de alta calidad para influir en políticas de salud y mejorar el bienestar global."
+        vision="Ser el centro de referencia en investigación y desarrollo de tecnologías médicas en América Latina."
+      />
+    </>
+  );
+};
 
 export default function AboutPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
-      <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 text-center">
-        <Construction className="w-16 h-16 text-yellow-500 mx-auto mb-4" />
-        <h1 className="text-3xl font-bold">{"Página 'Contact' en Construcción"}</h1>
-        <p className="mt-2 text-gray-600 dark:text-gray-400">
-          Estamos trabajando en esta sección. ¡Vuelve pronto para más
-          información!
-        </p>
-      </div>
-    </div>
+    <section className="mt-20">
+      <About />
+    </section>
   );
 }
