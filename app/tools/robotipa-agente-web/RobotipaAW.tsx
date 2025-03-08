0894afb4d6b8dcb2c59622543a731610355b3909
raulefdz <raulefdz@gmail.com>
2025-03-08 06:55:01 -0500

    WW

--- app/tools/robotipa-agente-web/RobotipaAW.tsx
+++ app/tools/robotipa-agente-web/RobotipaAW.tsx
@@ -278,30 +278,32 @@ const RobotipaAW = () => {
         </form>
       </div>
 
-      {isOpen  ??   <button
-        onClick={toggleChat}
-        className={`fixed bottom-6 right-6 bg-blue-500 text-white p-4 rounded-full shadow-xl hover:bg-blue-600 transition-all ${
-          isOpen ? "rotate-0" : ""
-        }`}
-        style={{ boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)" }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-            />
-          
-        </svg>
-      </button>}
+      {!isOpen && (
+  <button
+    onClick={toggleChat}
+    className={`fixed bottom-6 right-6 bg-blue-500 text-white p-4 rounded-full shadow-xl hover:bg-blue-600 transition-all ${
+      isOpen ? "rotate-0" : ""
+    }`}
+    style={{ boxShadow: "0 4px 12px rgba(0, 0, 0, 0.15)" }}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+      />
+    </svg>
+  </button>
+)}
+
+
     </div>
   );
 };
